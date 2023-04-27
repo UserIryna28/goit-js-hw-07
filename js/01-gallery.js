0d8653e24a4727afd3480977efeb10e39a879baf
UserIryna28 <Ltdcom@ukr.net>
2023-04-27 09:00:40 +0300

    add files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems)
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const galleryContainer = document.querySelector(".gallery")
+const imgMarcup = createGalleryItems(galleryItems)
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend',imgMarcup)
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join("");
+    
+}
+
+galleryContainer.addEventListener("click")
+
+function onGalleryContainer
